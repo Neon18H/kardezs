6a0335edf17f28aa5f4f6a50dc4d75b08b87974f
Juan14-04 <juandallow1@gmail.com>
2025-05-27 22:43:38 -0500

    se realizo aplicacion de Kardex

--- kardex_system/kardex/models.py
+++ kardex_system/kardex/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.conf import settings  # Importamos desde settings en lugar de directamente
 
-# Create your models here.
+class Compra(models.Model):
+    TIPO_MOVIMIENTO_CHOICES = [
+        ('C', 'Compra'),
+        ('D', 'Devolución'),
+        ('A', 'Ajuste'),
+    ]
+
+    usuario = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='compras')
+    fecha = models.DateTimeField(auto_now_add=True)
+    tipo_movimiento = models.CharField(max_length=1, choices=TIPO_MOVIMIENTO_CHOICES, default='C')
+    descripcion = models.CharField(max_length=100)
+    tienda = models.CharField(max_length=50)
+    monto = models.DecimalField(max_digits=10, decimal_places=2)
+
+    class Meta:
+        ordering = ['-fecha']
+
+    def save(self, args, **kwargs):
+        super().save(args, **kwargs)
+        self.usuario.actualizar_saldo()
